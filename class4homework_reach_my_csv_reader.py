78db019d9655b14efddfc1710c972a2984e66f07
mtl-lan <jocrebecca@gmail.com>
2020-02-11 23:58:10 -0500

    submit more homework

---
+++ class4homework_reach_my_csv_reader.py
@@ -0,0 +1,21 @@
+# Check if the boston housing dataset file path is really a file.
+# If it is print "I have a file to process", otherwise print "Boo, no file for me."
+# Read the boston housing dataset file
+# Print the dataset line by line
+# Modify this script by printing each line as a list of values.
+# Example: The line:abc,cde,efg,1,2,3
+# In the source dataset should become: ['abc','cde','efg','1','2','3']
+import os.path
+
+data_path = "/home/rebecca/PycharmProjects/python-notebook/housing.data"
+
+if os.path.isfile(data_path):
+    print("I have a file to process")
+else:
+    print("Boo, no file for me")
+
+with open(data_path) as f:
+    for line in f:
+        print(line.split())
+
+
